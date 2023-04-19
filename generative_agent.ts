0c658aefc5768808f3b85f4742ad6f8140011600
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-19 16:51:03 -0400

    more progress

--- generative_agent.ts
+++ generative_agent.ts
@@ -61,7 +61,7 @@ export class GenerativeAgent extends BaseModel {
         return lines.map(line => line.trim().replace(/^\\s*\\d+\\.\\s*/, '').trim());
     }
 
-    _computeAgentSummary(): string {
+    async _computeAgentSummary(): Promise<string> {
         const prompt = PromptTemplate.fromTemplate(
             "How would you summarize {name}'s core characteristics given the"
             + " following statements:\n"
@@ -71,7 +71,7 @@ export class GenerativeAgent extends BaseModel {
         );
 
         // The agent seeks to think about their core characteristics.
-        const relevantMemories: Memory[] = this.fetchMemories(`${this.name}'s core characteristics`);
+        const relevantMemories: Document[] = this.fetchMemories(`${this.name}'s core characteristics`);
         const relevantMemoriesStr: string = relevantMemories.map(mem => mem.pageContent).join('\n');
         const chain = new LLMChain({ llm: this.llm, prompt: prompt, verbose: this.verbose });
 
@@ -189,5 +189,175 @@ export class GenerativeAgent extends BaseModel {
      */
     fetchMemories(observation: string): Document[] {
         return this.memory_retriever.getRelevantDocuments(observation);
+    }
+
+    /**
+     * Return a descriptive summary of the agent.
+     */
+    async getSummary(force_refresh: boolean = false): Promise<string> {
+        const current_time = DateTime.local();
+        const since_refresh = (current_time.toMillis() - this.last_refreshed.toMillis()) * 1.0 / 1000;
+        if (!this.summary || since_refresh >= this.summary_refresh_seconds || force_refresh) {
+            this.summary = await this._computeAgentSummary();
+            this.last_refreshed = current_time;
+        }
+        return (
+            `Name: ${this.name} (age: ${this.age})`
+            + `\nInnate traits: ${this.traits}`
+            + `\n${this.summary}`
+        );
+    }
+
+    /**
+     * Return a full header of the agent's status, summary, and current time.
+     */
+    getFullHeader(force_refresh: boolean = false): string {
+        const summary = this.getSummary(force_refresh);
+        const current_time_str = new Date().toLocaleString("en-US", { month: "long", day: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" });
+        return `${summary}\nIt is ${current_time_str}.\n${this.name}'s status: ${this.status}`;
+    }
+
+    async _getEntityFromObservation(observation: string): Promise<string> {
+        const prompt = PromptTemplate.fromTemplate(
+            "What is the observed entity in the following observation? {observation}"
+            + "\nEntity="
+        );
+        const chain = new LLMChain({ llm: this.llm, prompt: prompt, verbose: this.verbose });
+        return (await chain.run({ observation: observation })).trim();
+    }
+
+    async _getEntityAction(observation: string, entity_name: string): Promise<string> {
+        const prompt = PromptTemplate.fromTemplate(
+            "What is the {entity} doing in the following observation? {observation}"
+            + "\nThe {entity} is"
+        );
+        const chain = new LLMChain({ llm: this.llm, prompt: prompt, verbose: this.verbose });
+        return (await chain.run({ entity: entity_name, observation: observation })).trim();
+    }
+
+    async _formatMemoriesToSummarize(relevant_memories: Document[]): Promise<string> {
+        const content_strs = new Set<string>();
+        const content: string[] = [];
+        for (const mem of relevant_memories) {
+            if (content_strs.has(mem.pageContent)) {
+                continue;
+            }
+            content_strs.add(mem.pageContent);
+            const created_time = new Date(mem.metadata.created_at).toLocaleString("en-US", { month: "long", day: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" });
+            content.push(`- ${created_time}: ${mem.pageContent.trim()}`);
+        }
+        return content.join("\n");
+    }
+
+    /**
+     * Summarize memories that are most relevant to an observation.
+     */
+    async summarizeRelatedMemories(observation: string): Promise<string> {
+        const entity_name = await this._getEntityFromObservation(observation);
+        const entity_action = this._getEntityAction(observation, entity_name);
+        // Fetch memories related to the agent's relationship with the entity
+        const q1 = `What is the relationship between ${this.name} and ${entity_name}`;
+        let relevant_memories = this.fetchMemories(q1);
+        // Fetch things related to the entity-action pair
+        const q2 = `${entity_name} is ${entity_action}`;
+        relevant_memories = relevant_memories.concat(this.fetchMemories(q2));
+        const context_str = await this._formatMemoriesToSummarize(relevant_memories);
+        const prompt = PromptTemplate.fromTemplate(
+            "{q1}?\nContext from memory:\n{context_str}\nRelevant context: "
+        );
+        const chain = new LLMChain({ llm: this.llm, prompt: prompt, verbose: this.verbose });
+        return (await chain.run({ q1: q1, context_str: context_str.trim() })).trim();
+    }
+
+    /**
+     * Reduce the number of tokens in the documents.
+     */
+    _getMemoriesUntilLimit(consumed_tokens: number): string {
+        const result: string[] = [];
+        for (const doc of this.memory_retriever.memory_stream.slice().reverse()) {
+            if (consumed_tokens >= this.max_tokens_limit) {
+                break;
+            }
+            consumed_tokens += this.llm.get_num_tokens(doc.pageContent);
+            if (consumed_tokens < this.max_tokens_limit) {
+                result.push(doc.pageContent);
+            }
+        }
+        return result.reverse().join("; ");
+    }
+
+    /**
+     * React to a given observation.
+     */
+    async _generateReaction(observation: string, suffix: string): Promise<string> {
+        const prompt = PromptTemplate.fromTemplate(
+            "{agent_summary_description}"
+            + "\nIt is {current_time}."
+            + "\n{agent_name}'s status: {agent_status}"
+            + "\nSummary of relevant context from {agent_name}'s memory:"
+            + "\n{relevant_memories}"
+            + "\nMost recent observations: {recent_observations}"
+            + "\nObservation: {observation}"
+            + "\n\n" + suffix
+        );
+        const agent_summary_description = this.getSummary();
+        const relevant_memories_str = this.summarizeRelatedMemories(observation);
+        const current_time_str = new Date().toLocaleString("en-US", { month: "long", day: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" });
+        const kwargs = {
+            agent_summary_description: agent_summary_description,
+            current_time: current_time_str,
+            relevant_memories: relevant_memories_str,
+            agent_name: this.name,
+            observation: observation,
+            agent_status: this.status
+        };
+        const consumed_tokens = this.llm.get_num_tokens(prompt.format({ recent_observations: "", ...kwargs }));
+        kwargs["recent_observations"] = this._getMemoriesUntilLimit(consumed_tokens);
+        const action_prediction_chain = new LLMChain({ llm: this.llm, prompt: prompt });
+        const result = await action_prediction_chain.run(kwargs);
+        return result.trim();
+    }
+
+    async generateReaction(observation: string): Promise<[boolean, string]> {
+        const call_to_action_template = (
+            "Should {agent_name} react to the observation, and if so,"
+            + " what would be an appropriate reaction? Respond in one line."
+            + ' If the action is to engage in dialogue, write:\nSAY: "what to say"'
+            + "\notherwise, write:\nREACT: {agent_name}'s reaction (if anything)."
+            + "\nEither do nothing, react, or say something but not both.\n\n"
+        );
+        const full_result = await this._generateReaction(observation, call_to_action_template);
+        const result = full_result.trim().split('\n')[0];
+        this.addMemory(`${this.name} observed ${observation} and reacted by ${result}`);
+        if (result.includes("REACT:")) {
+            const reaction = result.split("REACT:").pop().trim();
+            return [false, `${this.name} ${reaction}`];
+        }
+        if (result.includes("SAY:")) {
+            const said_value = result.split("SAY:").pop().trim();
+            return [true, `${this.name} said ${said_value}`];
+        } else {
+            return [false, result];
+        }
+    }
+
+    async generateDialogueResponse(observation: string): Promise<[boolean, string]> {
+        const call_to_action_template = (
+            'What would {agent_name} say? To end the conversation, write: GOODBYE: "what to say". Otherwise to continue the conversation, write: SAY: "what to say next"\n\n'
+        );
+        const full_result = await this._generateReaction(observation, call_to_action_template);
+        const result = full_result.trim().split('\n')[0];
+        if (result.includes("GOODBYE:")) {
+            const farewell = result.split("GOODBYE:").pop().trim();
+            this.addMemory(`${this.name} observed ${observation} and said ${farewell}`);
+            return [false, `${this.name} said ${farewell}`];
+        }
+        if (result.includes("SAY:")) {
+            const response_text = result.split("SAY:").pop().trim();
+            this.addMemory(`${this.name} observed ${observation} and said ${response_text}`);
+            return [true, `${this.name} said ${response_text}`];
+        } else {
+            return [false, result];
+        }
     }
 }
